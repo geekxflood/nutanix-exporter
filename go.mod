fdfc948c70f12c7d7eb4e3c830bd319701209d64
Christophe Rime <christophe.rime@eca-vaud.ch>
2023-03-29 15:34:30 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module nutanix-exporter
+
+go 1.20
+
+require github.com/tecbiz-ch/nutanix-go-sdk v0.1.15
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+
+	nutanix "github.com/tecbiz-ch/nutanix-go-sdk"
+)
+
+func main() {
+
+	configCreds := nutanix.Credentials{
+		Username: os.Getenv("NUTANIX_USERNAME"),
+		Password: os.Getenv("NUTANIX_PASSWORD"),
+	}
+
+	log.Printf("Username: %s", configCreds.Username)
+	log.Printf("Password: %s", configCreds.Password)
+	opts := []nutanix.ClientOption{
+		nutanix.WithCredentials(&configCreds),
+		nutanix.WithEndpoint("https://PC"),
+		nutanix.WithInsecure(),
+	}
+
+	client := nutanix.NewClient(opts...)
+
+	ctx := context.Background()
+	mycluster, err = client.Cluster.Get(ctx, "mycluster")
+
+	list, err = client.VM.All(ctx)
+
+}
